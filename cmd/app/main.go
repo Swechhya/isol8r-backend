b442a662dc6f2c48863d09f7f326d1cea9791be2
hastagurung <gurungryu@gmail.com>
2024-04-06 22:37:57 +0545

    router setup

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,45 +1,14 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/Swechhya/isol8r-backend/handlers"
-	"github.com/Swechhya/isol8r-backend/middleware"
+	"github.com/Swechhya/isol8r-backend/router"
 	"github.com/Swechhya/isol8r-backend/setup"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	setup.Setup()
 
-	router := gin.Default()
-	router.Use(middleware.CorsHandler())
-	router.Use(middleware.RcoveryHandler())
-
-	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Server Running...")
-	})
-
-	gh := router.Group("/gh")
-	{
-		gh.POST("/setup", handlers.SetupGithub)
-		gh.GET("/repos", handlers.GetRepos)
-		gh.GET("/branches/:repoId", handlers.GetBranches)
-		gh.POST("/save-env/:repoId", handlers.UploadEnvFile)
-	}
-
-	//feature-enviroment-handler
-	featureEnvironment := router.Group("/fe")
-	{
-		featureEnvironment.GET("/repos", handlers.FEGetRepoHandler)
-		featureEnvironment.GET("/list", handlers.FEListHandler)
-		featureEnvironment.POST("/create", handlers.FECreateHandler)
-		featureEnvironment.POST("/delete/:id", handlers.FEDeleteHandler)
-		featureEnvironment.POST("/redeploy/:id", handlers.FERedeployHandler)
-		featureEnvironment.POST("/edit/:id", handlers.FEEditHandler)
-	}
-
-	// By default it serves on :8080 unless a
-	// PORT environment variable was defined.
-	router.Run()
+	//setup router
+	r := router.SetupRouter()
+	r.Run()
 }
