7597a459c45eed31ec49a1c6a99024b69f886f2c
Lucky Shrestha <luckshrestha@gmail.com>
2024-04-07 00:01:05 +0545

    Fix

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/Swechhya/isol8r-backend/router"
-	"github.com/Swechhya/isol8r-backend/services"
 	"github.com/Swechhya/isol8r-backend/setup"
 )
 
@@ -18,23 +14,23 @@ func main() {
 	r.Run()
 }
 
-func TestRun() error {
+// func TestRun() error {
 
-	ecr := "654654451390.dkr.ecr.us-east-1.amazonaws.com/test:"
+// 	ecr := "654654451390.dkr.ecr.us-east-1.amazonaws.com/test:"
 
-	repoName := strings.Split("Swechhya/Contact-Manager-Frontend", "/")[1]
-	dest := fmt.Sprintf("%s%s-%s", ecr, "feature-new-test", repoName)
-	err := services.GenerateBuildManifest("feature-new-test", "Swechhya/Contact-Manager-Frontend", "main", dest)
-	if err != nil {
-		return err
-	}
-	err = services.GenerateDeployManifest("feature-new-test", dest)
-	if err != nil {
-		return err
-	}
-	err = services.DeployEnvironment("feature-new-test")
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// 	repoName := strings.Split("Swechhya/Contact-Manager-Frontend", "/")[1]
+// 	dest := fmt.Sprintf("%s%s-%s", ecr, "feature-new-test", repoName)
+// 	err := services.GenerateBuildManifest("feature-new-test", "Swechhya/Contact-Manager-Frontend", "main", dest)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	err = services.GenerateDeployManifest("feature-new-test", dest, nil)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	err = services.DeployEnvironment("feature-new-test")
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
